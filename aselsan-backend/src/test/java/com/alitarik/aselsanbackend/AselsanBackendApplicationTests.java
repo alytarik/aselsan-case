05bf7e444f6105731c67c190252f25dc8cb935d7
Ali Tarik <alimirik55@gmail.com>
2023-09-25 18:19:42 +0300

    initial commit

---
+++ aselsan-backend/src/test/java/com/alitarik/aselsanbackend/AselsanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alitarik.aselsanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AselsanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
