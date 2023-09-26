b3cd33b80318d6acf0856192eeebaa878964dd4b
Ali Tarik <alimirik55@gmail.com>
2023-09-26 10:34:21 +0300

    cors settings

--- aselsan-backend/src/main/java/com/alitarik/aselsanbackend/AselsanBackendApplication.java
+++ aselsan-backend/src/main/java/com/alitarik/aselsanbackend/AselsanBackendApplication.java
@@ -2,18 +2,32 @@ package com.alitarik.aselsanbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
 public class AselsanBackendApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(AselsanBackendApplication.class, args);
-    }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(AselsanBackendApplication.class, args);
+  }
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
+
+  @Bean
+  public WebMvcConfigurer configure() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry reg) {
+        reg.addMapping("/**").allowedOrigins("*");
+      }
+    };
+  }
 }
