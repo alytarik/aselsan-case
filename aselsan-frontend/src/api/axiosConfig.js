f09572d4830f4c3e0ca1577b0a7c80c32bb8b065
Ali Tarik <alimirik55@gmail.com>
2023-10-01 17:48:52 +0300

    add fallback api

--- aselsan-frontend/src/api/axiosConfig.js
+++ aselsan-frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //get base url from environment variable
-const baseURL = process.env.REACT_APP_API_URL || 'https://aselsan-backend.onrender.com/api/v1/';
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1/';
 
 export default axios.create({
     baseURL: baseURL
