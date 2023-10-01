67bb42912a3a302463baf653871795b7b9f0e4a4
Ali Tarik <alimirik55@gmail.com>
2023-10-01 12:54:03 +0300

    change admin path

--- aselsan-frontend/src/index.js
+++ aselsan-frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Admin from './Admin';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -15,7 +15,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route index element={<App />} />
-        <Route path="admin" element={<Admin />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
