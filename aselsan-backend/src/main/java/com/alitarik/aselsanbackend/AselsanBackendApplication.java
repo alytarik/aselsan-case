32ded99d7b1e936cd6b3493332a3b308fa28df8a
Ali Tarik <alimirik55@gmail.com>
2023-10-01 18:01:20 +0300

    add scheduled temperature checking

--- aselsan-backend/src/main/java/com/alitarik/aselsanbackend/AselsanBackendApplication.java
+++ aselsan-backend/src/main/java/com/alitarik/aselsanbackend/AselsanBackendApplication.java
@@ -3,6 +3,7 @@ package com.alitarik.aselsanbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class AselsanBackendApplication {
   public static void main(String[] args) {
     SpringApplication.run(AselsanBackendApplication.class, args);
---
+++ aselsan-backend/src/main/java/com/alitarik/aselsanbackend/service/TemperatureService.java
@@ -0,0 +1,25 @@
+package com.alitarik.aselsanbackend.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TemperatureService {
+
+    @Scheduled(fixedRate = 5000)
+    public void checkTemperature() {
+        System.out.println("====================================");
+        System.out.println("Checking the temperature");
+        int temperature = (int) (Math.random() * 5 + 16);
+        int idealTemperature = 18;
+        System.out.println("Temperature is " + (temperature - idealTemperature) + " C off.");
+        if (temperature - idealTemperature < -1) {
+            System.out.println("Turning off the cooler");
+        } else if (temperature - idealTemperature > 1) {
+            System.out.println("Turning on the cooler");
+        } else {
+            System.out.println("Temperature is ideal");
+        }
+        System.out.println("====================================");
+    }
+}
