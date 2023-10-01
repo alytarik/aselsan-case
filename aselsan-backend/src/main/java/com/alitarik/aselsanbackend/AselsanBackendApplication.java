e68df09b75b2d44074d64da0f8c859778e93321e
Ali Tarik <alimirik55@gmail.com>
2023-10-01 18:54:16 +0300

    remove temp endpoint

--- aselsan-backend/src/main/java/com/alitarik/aselsanbackend/AselsanBackendApplication.java
+++ aselsan-backend/src/main/java/com/alitarik/aselsanbackend/AselsanBackendApplication.java
@@ -4,23 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
 @RestController
 @EnableScheduling
+@OpenAPIDefinition
 public class AselsanBackendApplication {
   public static void main(String[] args) {
     SpringApplication.run(AselsanBackendApplication.class, args);
-  }
-
-  @GetMapping("/hello")
-  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    return String.format("Hello %s!", name);
   }
 
   @Bean
